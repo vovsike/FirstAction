a6da5a4831c51996e5121ccad90d9a731f7bffd9
Vladimir <vladimir.garanin@stfc.ac.uk>
2021-09-23 11:53:48 +0000

    Add tree list files

--- main.py
+++ main.py
@@ -2,9 +2,10 @@ import os
 
 
 def main():
-    path_to_search = os.listdir("/github/workspace/docker")
-    file_locaiton = find_files("Dockerfile", "path_to_search")
-    print(f"::set-output name=myOutput::{path_to_search}")
+    list_files("/github")
+    # path_to_search = os.listdir("/github/home")
+    # file_locaiton = find_files("Dockerfile", "path_to_search")
+    # print(f"::set-output name=myOutput::{path_to_search}")
 
 def find_files(filename, search_path):
    result = []
@@ -13,6 +14,15 @@ def find_files(filename, search_path):
       if filename in files:
          result.append(os.path.join(root, filename))
    return result
+
+def list_files(startpath):
+    for root, dirs, files in os.walk(startpath):
+        level = root.replace(startpath, '').count(os.sep)
+        indent = ' ' * 4 * (level)
+        print('{}{}/'.format(indent, os.path.basename(root)))
+        subindent = ' ' * 4 * (level + 1)
+        for f in files:
+            print('{}{}'.format(subindent, f))
 
 if __name__ == "__main__":
     main()
