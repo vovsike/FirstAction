a0c5b82fb1149d2746937557ac1a79a7f8adc171
Vladimir <vladimir.garanin@stfc.ac.uk>
2021-09-23 11:10:14 +0000

    Add listdir to python

--- main.py
+++ main.py
@@ -2,12 +2,8 @@ import os
 
 
 def main():
-    my_input = os.environ["INPUT_MYINPUT"]
-
-    my_output = f"Hello {my_input}"
-
+    my_output = os.listdir()
     print(f"::set-output name=myOutput::{my_output}")
-
 
 if __name__ == "__main__":
     main()
