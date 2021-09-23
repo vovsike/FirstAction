888cfb84ac53b0062f556be0357fd32e3afc6d90
Vladimir <vladimir.garanin@stfc.ac.uk>
2021-09-23 11:22:50 +0000

    Set output to list github dir

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import os
 
 
 def main():
-    my_output = os.listdir('/github/workspace')
+    my_output = os.listdir('/github')
     print(f"::set-output name=myOutput::{my_output}")
 
 if __name__ == "__main__":
