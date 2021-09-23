f4d3a23ff192d7f3e178c2e80dc92154b56c2e69
Vladimir <vladimir.garanin@stfc.ac.uk>
2021-09-23 12:18:20 +0000

    Another try of search funciton

--- main.py
+++ main.py
@@ -2,29 +2,13 @@ import os
 
 
 def main():
-    print(list_files("/github"))
-    path_to_search = "/github/workspace"
-    print(path_to_search)
-    file_locaiton = find_files("Dockerfile", "path_to_search")
-    print(file_locaiton)
-    print(f"::set-output name=myOutput::{file_locaiton}")
+    file = find("Dockerfile", "/github/workspace")
+    # print(f"::set-output name=myOutput::{file_locaiton}")
 
-def find_files(filename, search_path):
-   result = []
-
-   for root, dir, files in os.walk(search_path):
-      if filename in files:
-         result.append(os.path.join(root, filename))
-   return result
-
-def list_files(startpath):
-    for root, dirs, files in os.walk(startpath):
-        level = root.replace(startpath, '').count(os.sep)
-        indent = ' ' * 4 * (level)
-        print('{}{}/'.format(indent, os.path.basename(root)))
-        subindent = ' ' * 4 * (level + 1)
-        for f in files:
-            print('{}{}'.format(subindent, f))
+def find(name, path):
+    for root, dirs, files in os.walk(path):
+        if name in files:
+            return os.path.join(root, name)
 
 if __name__ == "__main__":
     main()
