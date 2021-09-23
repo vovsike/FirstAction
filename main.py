01445faf16c9e6ebae373dbd309a6d6725e1222f
Vladimir <vladimir.garanin@stfc.ac.uk>
2021-09-23 11:31:00 +0000

    Add search functionality

--- main.py
+++ main.py
@@ -2,8 +2,17 @@ import os
 
 
 def main():
-    my_output = os.listdir('/github')
-    print(f"::set-output name=myOutput::{my_output}")
+    path_to_search = "/github"
+    file_locaiton = find_files("Dockerfile", "path_to_search")
+    print(f"::set-output name=myOutput::{file_locaiton}")
+
+def find_files(filename, search_path):
+   result = []
+
+   for root, dir, files in os.walk(search_path):
+      if filename in files:
+         result.append(os.path.join(root, filename))
+   return result
 
 if __name__ == "__main__":
     main()
