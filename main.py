cb14ecf82f8ee21d62eba7c7f1b4789e54912294
Vladimir <vladimir.garanin@stfc.ac.uk>
2021-09-23 11:20:24 +0000

    Set output to list worksapce dir

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import os
 
 
 def main():
-    my_output = os.listdir('/')
+    my_output = os.listdir('/github/workspace')
     print(f"::set-output name=myOutput::{my_output}")
 
 if __name__ == "__main__":
