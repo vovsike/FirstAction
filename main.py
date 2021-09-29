375db2b7dcf5754e02f7ae7ad86c6611e63eb88a
Vladimir <vladimir.garanin@stfc.ac.uk>
2021-09-29 09:38:54 +0000

    Output data in JSON format

--- main.py
+++ main.py
@@ -1,12 +1,16 @@
 import os
+import json
 
 def main():
     # file = find("Dockerfile", "/github/workspace/")
     # relative_path = "/github/workspace/"
     # dockerfilelocaiton = (os.path.relpath(file, relative_path))
     # print(f"::set-output name=myOutput::{dockerfilelocaiton}")
-    new_l = [1,2,3]
-    print(f"::set-output name=myOutput::{new_l}")
+    new_l = {
+        "Image name" : "DockerOne",
+        "Image path" : "some/path"
+    }
+    print(f"::set-output name=myOutput::{json.dumps(new_l)}")
 
 def find(name, path):
     for root, dirs, files in os.walk(path):
