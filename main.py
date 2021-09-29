713abd0555cf640ae99e5e56eff2d78ae0cd4f2e
Vladimir <vladimir.garanin@stfc.ac.uk>
2021-09-29 08:39:00 +0000

    Remove unwanted parameters from yaml, change python script to return a list

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 import os
 
 def main():
-    file = find("Dockerfile", "/github/workspace/")
-    relative_path = "/github/workspace/"
-    dockerfilelocaiton = (os.path.relpath(file, relative_path))
-    print(f"::set-output name=myOutput::{dockerfilelocaiton}")
+    # file = find("Dockerfile", "/github/workspace/")
+    # relative_path = "/github/workspace/"
+    # dockerfilelocaiton = (os.path.relpath(file, relative_path))
+    # print(f"::set-output name=myOutput::{dockerfilelocaiton}")
+    new_l = [1,2,3]
+    print(f"::set-output name=myOutput::{new_l}")
 
 def find(name, path):
     for root, dirs, files in os.walk(path):
