2b7724a680f5c0e4716346f92db0fd875adcaa88
Vladimir <vladimir.garanin@stfc.ac.uk>
2021-10-01 13:28:46 +0000

    Make loop to add all files to list

--- main.py
+++ main.py
@@ -2,13 +2,19 @@ import os
 import json
 
 def main():
+    new_l = []
     files_list = find("Dockerfile", "/github/workspace/")
     print(files_list)
+    for item in files_list:
+        docker_dict={}
+        docker_dict["DockerFile"] = item
+        new_l.append(docker_dict)
     # relative_path = "/github/workspace/"
     # dockerfilelocaiton = (os.path.relpath(file, relative_path))
     # print(f"::set-output name=myOutput::{dockerfilelocaiton}")
-    new_l = [{"DockerName":"docker/Dockerfile"},{"DockerName":"docker/Dockerfile"}]
-    print(f"::set-output name=myOutput::{list(new_l)}")
+    print(new_l)
+    # new_l = [{"DockerName":"docker/Dockerfile"},{"DockerName":"docker/Dockerfile"}]
+    # print(f"::set-output name=myOutput::{list(new_l)}")
 
 
 def find(name, path):
