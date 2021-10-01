0da4000caee9957054f1e901d9fe0ef8c3a2a5e2
Vladimir <vladimir.garanin@stfc.ac.uk>
2021-10-01 13:08:37 +0000

    Search file as list

--- main.py
+++ main.py
@@ -3,16 +3,20 @@ import json
 
 def main():
     # file = find("Dockerfile", "/github/workspace/")
+    print(find("Dockerfile", "/github/workspace/"))
     # relative_path = "/github/workspace/"
     # dockerfilelocaiton = (os.path.relpath(file, relative_path))
     # print(f"::set-output name=myOutput::{dockerfilelocaiton}")
     new_l = [{"DockerName":"docker/Dockerfile"},{"DockerName":"docker/Dockerfile"}]
     print(f"::set-output name=myOutput::{list(new_l)}")
 
+
 def find(name, path):
+    files_l = []
     for root, dirs, files in os.walk(path):
         if name in files:
-            return os.path.join(root, name)
+            files_l.append(os.path.join(root, name))
+    return files_l
 
 if __name__ == "__main__":
     main()
